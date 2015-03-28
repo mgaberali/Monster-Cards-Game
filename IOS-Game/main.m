b5105c365f2e42e0aac6028359952cb8e7ec58f5
JETS <participant@Ahmeds-Mac.local>
2015-03-28 19:18:13 +0200

    Initial Commit

---
+++ IOS-Game/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOS-Game
+//
+//  Created by JETS on 3/28/15.
+//  Copyright (c) 2015 Mad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAppDelegate class]));
+    }
+}

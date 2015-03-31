c7d9a909a8a417fd38cd9e670c08ecd00c5115fb
JETS <participant@Ahmeds-Mac.local>
2015-03-31 23:32:14 +0200

    Initial Commit

---
+++ IOS_Game_App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOS_Game_App
+//
+//  Created by JETS on 3/31/15.
+//  Copyright (c) 2015 JETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOS_Game_App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IOS_Game_App
+//
+//  Created by JETS on 3/31/15.
+//  Copyright (c) 2015 JETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

b091e5d5bbf3f01c1c562f4784d629464a5a270d
JETS <participant@192.168.74.128>
2015-04-03 17:02:21 +0200

    Add Share on Facebook Functionality

---
+++ IOS-Game/MSocialShareUtility.h
@@ -0,0 +1,17 @@
+//
+//  MSocialShareUtility.h
+//  IOS-Game
+//
+//  Created by JETS on 4/3/15.
+//  Copyright (c) 2015 Mad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
+
+@interface MSocialShareUtility : NSObject
+
++ (void) sharePostOnFB: (NSString *) postString withImage: (UIImage*) image fromViewCotroller: (UIViewController *) viewController;
+
+@end
---
+++ IOS-Game/MSocialShareUtility.m
@@ -0,0 +1,43 @@
+//
+//  MSocialShareUtility.m
+//  IOS-Game
+//
+//  Created by JETS on 4/3/15.
+//  Copyright (c) 2015 Mad. All rights reserved.
+//
+
+#import "MSocialShareUtility.h"
+
+@implementation MSocialShareUtility
+
+/*
+ * share score on facebook
+ */
++ (void) sharePostOnFB: (NSString *) postString withImage: (UIImage*) image fromViewCotroller: (UIViewController *) viewController{
+    
+    SLComposeViewController *controller = [SLComposeViewController
+                                           composeViewControllerForServiceType:SLServiceTypeFacebook];
+    SLComposeViewControllerCompletionHandler myBlock =
+    ^(SLComposeViewControllerResult result){
+        if (result == SLComposeViewControllerResultCancelled)
+        {
+            NSLog(@"Cancelled");
+        }
+        else
+        {
+            NSLog(@"Done");
+        }
+        [controller dismissViewControllerAnimated:YES completion:nil];
+    };
+    controller.completionHandler =myBlock;
+    //Adding the Text to the facebook post value from iOS
+    [controller setInitialText:postString];
+    //Adding the URL to the facebook post value from iOS
+    [controller addImage:image];
+    //Adding the Text to the facebook post value from iOS
+    [viewController presentViewController:controller animated:YES completion:nil];
+    
+}
+
+
+@end
